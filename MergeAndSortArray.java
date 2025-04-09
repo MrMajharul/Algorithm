7a8bf9c7e9acde4a76dc9c85ba01c02c3a4c4535
Majharul <158082213+MrMajharul@users.noreply.github.com>
2025-04-09 09:42:28 +0600

    Add files via upload
    This is my lab task. 
    Took arr1 then sort this array and took another array then combine this and then sorted again.
    
    Here I first took a random array, then sorted it and took another array, then I added the two together and sorted the combine array again.

---
+++ MergeAndSortArray.java
@@ -0,0 +1,28 @@
+import java.util.Arrays;
+import java.util.Random;
+
+public class MergeAndSortArray {
+    public static void main(String[] args) {
+        Random rand = new Random();
+        int[] arr1 = new int[10];
+        for (int i = 0; i < arr1.length; i++) {
+            arr1[i] = rand.nextInt(100); 
+        }
+        int[] arr2 = new int[10];
+        for (int i = 0; i < arr2.length; i++) {
+            arr2[i] = rand.nextInt(100);
+        }
+        int[] combined = new int[arr1.length + arr2.length];
+        System.arraycopy(arr1, 0, combined, 0, arr1.length);
+        System.arraycopy(arr2, 0, combined, arr1.length, arr2.length);
+
+        System.out.println("First array:  " + Arrays.toString(arr1));
+        System.out.println("Sorted array:");
+        System.out.println(Arrays.toString(arr1));
+        System.out.println("Second array: " + Arrays.toString(arr2));
+        System.out.println("Combined array: " + Arrays.toString(combined));
+
+        Arrays.sort(combined);
+        System.out.println("Sorted combined array: " + Arrays.toString(combined));
+    }
+}
